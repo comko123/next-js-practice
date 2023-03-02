b7587000ebf3362bf4524dd7b475094d0723684f
Minseok Kim <comko123@naver.com>
2023-03-03 00:43:42 +0900

    render_second

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,14 +1,12 @@
 import axios from "axios"
-import { movie } from "."
+import { GetStaticPaths, GetStaticProps, NextPage } from "next"
 import Image from "next/image"
-import useSWR,{ SWRConfig } from "swr"
 import { useRouter } from "next/router"
-import { GetServerSideProps, NextPage } from "next"
+import { movie } from "."
 
-const DetailRender:NextPage = () => {
-    const {query:{id},replace} = useRouter()
-    const {data} = useSWR(`/movie/${id}`)
-    return(<>{data?<div className="grid grid-cols-2 gap-3 m-3 ">
+const Detail:NextPage<{data:movie}> = ({data}) => {
+    const {replace} = useRouter()
+    return<>{data?<div className="grid grid-cols-2 gap-3 m-3 ">
     <Image width={3000} height={30} alt=""  priority
     src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path as string}`}
     className="h-[95vh] aspect-square rounded-2xl cursor-pointer"/>
@@ -16,7 +14,7 @@ const DetailRender:NextPage = () => {
     <div>
         <h1 className="text-center font-bold text-xl pb-5 mb-5 border-b-2 border-b-black">{data.title}</h1>
         <div className="flex justify-end mr-5 font-bold text-md"> adult : {JSON.stringify(data.adult)}</div>
-        <div className="flex justify-end mr-5 font-bold text-md pb-5 mb-5 border-b-2 border-b-black"> popularity : {Math.ceil(data.popularity)}</div>
+        <div className="flex justify-end mr-5 font-bold text-md pb-5 mb-5 border-b-2 border-b-black"> popularity : {Math.ceil(data.popularity as number)}</div>
         <span className="font-bold text-2xl">overview : </span>
         <span className=" mr-5 font-bold text-md text-justify">{data.overview} </span>
         <div className="flex w-full h-[40vh] items-center justify-center">
@@ -24,21 +22,20 @@ const DetailRender:NextPage = () => {
             onClick = {()=>replace('/')}>home</button></div>
     </div>
     
-    </div>:"loading..."}</>)
+    </div>:"loading..."}</>
 }
 
-const Detail:NextPage<movie> = ({data,detailLink}) => {
-    //key url을 동적으로 생성하여 영화 디테일 페이지에 접속 할때마다 해당 url의 캐시를 채워준다.
-return(<SWRConfig value={{fallback:{[detailLink as string]:data}}}>
-    <DetailRender/>
-</SWRConfig>)
+export const getStaticPaths:GetStaticPaths= async() => {
+    // 몇개의 페이지가 있는지 next에게 알려주는 과정 
+    // 아래의 코드의 patsh는 length가 20개이므로 20개의 detail페이지가 생성된다.
+    const {results} = await(await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.APP_KEY}`)).data 
+    const paths = results.map(({id}:{id:string})=>{return{params:{id:id+""}}})
+   return({paths,fallback:false})
 }
 
-export const getServerSideProps:GetServerSideProps = async({params}) => {
-    //영화마다 id값이 다르기 때문에 key url을 동적으로 생성하기 위해 링크도 같이 넘긴다.
-const detailLink = `/movie/${params?.id}`
-const data = await(await axios(`http://localhost:3000/movie/${params?.id}`)).data
-    return({props:{data,detailLink}})
+export const getStaticProps:GetStaticProps = async({params}) => {
+    const data = await(await axios(`https://api.themoviedb.org/3/movie/${params?.id}?api_key=10923b261ba94d897ac6b81148314a3f`)).data
+    return({props:{data}})
 }
 
 export default Detail
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,27 @@
 import axios from "axios"
-import Link from "next/link"
+import { GetStaticProps, NextPage } from "next"
 import Image from "next/image"
-import useSWR , { SWRConfig } from "swr"
-import { GetServerSideProps, NextPage } from "next"
+import Link from "next/link"
 
-export interface movie {[key:string]:string|number|number[]}
+export interface movie {[key:string]:string|number|string[]|number[]}
 
-const Movie:NextPage = () => {
-const {data:{results}} = useSWR("/movie/all") 
-//Main 컴포넌트에서 기본값을 설정했기 때문에 데이터가 비어있는 상태가 아닌 상태로 시작한다.
-return(<div className="grid grid-cols-5 my-3">
-{results?.map((item:movie)=><Link href={`/${item.id}`} key={item.id as string}>
-<Image width={3000} height={30} alt="" priority
-className="w-52 aspect-square my-2 shadow-xl rounded-xl mx-auto hover:scale-x-105 transition-transform cursor-pointer" 
-src={`https://image.tmdb.org/t/p/w500/${item.poster_path as string}`}/>
-</Link>)}
-</div>)
-}
-
-const Main:NextPage<movie> = ({results}) => {
-    //불러온 데이터를 props로 받아 swr키에 기본값으로 설정한다.
-    return(<SWRConfig value={{fallback:{"/movie/all":results}}}>
-        <Movie/>
-    </SWRConfig>)
+const Main:NextPage<{results:movie[]}> = ({results}) => {
+        return (<div className="grid grid-cols-5 my-3">
+        {results.map((item:any)=>{
+                return(<Link href={`/${item.id}`} key={item.id as string}>
+                <Image width={3000} height={30} alt="" priority
+                className="w-52 aspect-square my-2 shadow-xl rounded-xl mx-auto hover:scale-x-105 transition-transform cursor-pointer" 
+                src={`https://image.tmdb.org/t/p/w500/${item.poster_path as string}`}/>
+                </Link>)})}</div>)
 } 
 
-export const getServerSideProps:GetServerSideProps = async() => {
-    //sever-side에서 데이터를 불러온다.
-    const {results} = await(await axios("http://localhost:3000/movie/all")).data
-return({props:{results}})
+
+export const getStaticProps:GetStaticProps = async() => {
+        //항상 서버에서 실행되고 클라이언트에서는 실행되지 않는다.
+        //빌드된 후 페이지 내부에 데이터가 바뀌더라도 페이지는 바뀌지 않는다. (정적 페이지(html)로 빌드되기 때문)
+        //파일 안에서 getStaticProps라는 함수를 export 하는 경우 해당함수에서 반환된props를 사용하여 빌드시 페이지를 사전에 렌더링한다.
+        const {results} = await(await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.APP_KEY}`)).data 
+        return{props:{results}}
 }
 
 export default Main
