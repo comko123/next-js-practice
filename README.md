e0da88b0c282075d98c31561f8df52e4bf73f423
Minseok Kim <comko123@naver.com>
2023-03-03 00:55:34 +0900

    render_first

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # ssr + cache 
+- 유저의 요청에따라 페이지를 서버단에서 재렌더링 하여 클라이언트단에서는 부담을 최소화 하는 방식  
 
 - get server side props와 swr의 cache기능을 사용한 영화 페이지 예시 
 
+## get server side props 
 - get server side props는 서버단에서 요청을 하고 요청이 완료되면 render을 하기때문에 loading state가필요없다.
 
- 
+- 서버단에서 요청을 먼저하고 사용자는 완료된 결과물만 보기 때문에 loading state가 필요없는 대신 사용자는 서버단에서
+요청이 완료 될때까지 아무것도 볼수 없다. (google에서는 이러한 방식을 사용중) 
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,8 @@ export interface movie {[key:string]:string|number|number[]}
 const Movie:NextPage = () => {
 const {data:{results}} = useSWR("/movie/all") 
 //Main 컴포넌트에서 기본값을 설정했기 때문에 데이터가 비어있는 상태가 아닌 상태로 시작한다.
+/*SWR를 사용하면서, SEO를 위해 페이지를 미리 렌더링할 수 있으며 
+클라이언트 측에서 caching, revalidation, focus tracking, refetching와 같은 기능도 사용 가능하다.*/
 return(<div className="grid grid-cols-5 my-3">
 {results?.map((item:movie)=><Link href={`/${item.id}`} key={item.id as string}>
 <Image width={3000} height={30} alt="" priority
@@ -27,6 +29,7 @@ const Main:NextPage<movie> = ({results}) => {
 
 export const getServerSideProps:GetServerSideProps = async() => {
     //sever-side에서 데이터를 불러온다.
+    //페이지에서 getServerSideProps라는 함수를 export 하면 Next.js는 해당 함수에서 반환된 데이터를 사용하여 각 요청에서 이 페이지를 미리 랜더링한다.
     const {results} = await(await axios("http://localhost:3000/movie/all")).data
 return({props:{results}})
 }
