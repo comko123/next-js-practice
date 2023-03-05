445c2e9d5d604c23fe44b181fa72686f0d636990
Minseok Kim <comko123@naver.com>
2023-03-06 00:23:30 +0900

    render_third

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { GetStaticProps, NextPage } from "next"
 import Image from "next/image"
-import Link from "next/link"
 import { useRouter } from "next/router"
 
 export interface movie {[key:string]:string|number|string[]|number[]}
