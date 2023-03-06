10bf2e43f313771fb929e207750bd57cfc550925
Minseok Kim <comko123@naver.com>
2023-03-06 16:47:22 +0900

    add to comment

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -28,14 +28,41 @@ const Detail:NextPage<{data:movie}> = ({data}) => {
 export const getStaticPaths: GetStaticPaths = () => {
     return {
       paths: [],
-      fallback: "blocking",
+      /*SSG에서는 paths라는 필드에 몇개에 페이지가 필요한지 알려주는 과정이 있었지만 
+        ISR에서는 empty array를 주는 것으로 페이지를 미리 생성하지 않고 사용자가 방문했을때만 
+        해당 detail페이지를 생성하는 것을 의미한다.*/
+     fallback: "blocking",
+     /*fallback옵션 
+     fallback는 getStaticPaths에서 리턴하지 않은 페이지에 접속 시 발생하는 동작에 관련한 옵션인데 3가지 옵션이있다.
+     true : getStaticPaths에서 리턴하지 않은 페이지에 접속 시, 먼저 사용자에게 fallback 페이지를 보여준다.
+     서버에서 static하게 페이지를 생성하며 ,해당 페이지를 사용자에게 보여준다.
+     다음부터 해당 페이지로 접속하는 사용자에게는 static한 페이지를 보여준다.
+     false : getStaticPaths에서 리턴하지 않은 페이지는 모두 404로 연결한다.
+     "blocking": getStaticPaths에서 리턴하지 않은 페이지에 접속 시, 사용자에게 server side rendering한 static 페이지를 보여준다.
+     다음부터 해당 페이지로 접속하는 사용자에게는 server side rendering한 static 페이지를 보여준다.
+     즉 fallback 페이지나 로딩 화면이 없다.
+     */
     };
   };
 
 export const getStaticProps:GetStaticProps = async({params}) => {
+    
     await new Promise((resolve) => setTimeout(resolve, 10000))
-    const data = await(await axios(`http://localhost:3000/movie/${params?.id}`)).data
-    return({props:{data}})
+  /*위코드는 페이지 렌더링을 10초 지연시키는 코드이며 ISR의 특성상 사용자가 해당페이지에 
+    처음 방문할때는 위 코드로 인하여 10초지연이 발생 하지만  그이후 페이지를 다시 방문 하거나 
+    해당 페이지에서 reload를 하게 된다면 10초 지연이 발생 하지 않는다.
+    그이유는 해당 페이지를 처음 방문했을때 10초 지연이 발생한후 
+    정적(static)인 html파일로 build하기 때문에 지연이 발생하지 않는다.
+    (해당페이지를 처음 이후에 cache하기때문.)
+   */ 
+  const data = await(await axios(`http://localhost:3000/movie/${params?.id}`)).data
+    return({props:{data},
+      revalidate:20
+    /*revalidate 는 html을 regenerate 할 수 있게하는 옵션이다.
+      html이 만들어지고 20초 동안은 사용자에게 같은 html파일을 그대로 응답한다. 
+      20초가 지나고 GET 요청이 오면, 기존 html을 응답하면서 동시에 새로운 html을 regenerate 한다. 
+      그리고 다음 GET 요청부터는 갱신된 html 문서를 응답한다.*/
+    })
 }
 
 export default Detail
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,11 @@ const Main:NextPage<{results:movie[]}> = ({results}) => {
         return (<div className="grid grid-cols-5 my-3">
         {results.map((item)=>{
                 return(<div onClick={()=>push(`/${item.id}`)} key={item.id as string}>
+                {/* onClick대신 Link를 사용하는 방법이있는데 Link를 사용하면 SSG처럼 모든detail페이지 (20개)가 
+                메인페이지를 방문하면 미리 생겨버린다. 
+                아마 next-js에서 제공하는 Link가 다른 detail페이지들과 연동이 되어 있는것같다.
+                그로인해 위와같은 현상이 발생하는것 같다. 
+                */}
                 <Image width={3000} height={30} alt="" priority
                 className="w-52 aspect-square my-2 shadow-xl rounded-xl mx-auto hover:scale-x-105 transition-transform cursor-pointer" 
                 src={`https://image.tmdb.org/t/p/w500/${item.poster_path as string}`}/>
@@ -22,7 +27,7 @@ export const getStaticProps:GetStaticProps = async() => {
         //빌드된 후 페이지 내부에 데이터가 바뀌더라도 페이지는 바뀌지 않는다. (정적 페이지(html)로 빌드되기 때문)
         //파일 안에서 getStaticProps라는 함수를 export 하는 경우 해당함수에서 반환된props를 사용하여 빌드시 페이지를 사전에 렌더링한다.
         const {results} = await(await axios(`http://localhost:3000/movie/all`)).data 
-        return{props:{results}}
+        return{props:{results},revalidate:20}
 }
 
 export default Main
